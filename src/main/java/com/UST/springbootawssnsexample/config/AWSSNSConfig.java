de2f4c9eec6bf28912d217038e5d04d6bebd3367
ameena08 <ameenaami101@gmail.com>
2023-06-01 09:08:32 +0530

    Initial commit

---
+++ src/main/java/com/UST/springbootawssnsexample/config/AWSSNSConfig.java
@@ -0,0 +1,25 @@
+package com.UST.springbootawssnsexample.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+@Configuration
+public class AWSSNSConfig {
+   public static final String SECRET_KEY = "9PefLnn9rkfa/X9o+D6dGeaBjXXVX3GYKZvW/Z+J";
+    public static final String ACCESS_KEY = "AKIAUXFBZBUKKWQWJZGU";
+
+    @Primary
+    @Bean
+    public AmazonSNSClient getSnsClient() {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY,
+                        SECRET_KEY)))
+                .build();
+    }
+
+}
